e164a3d872c07cca41822a67902b464b39545bfb
Iqbal Rizky <arizkyiqbal@gmail.com>
2023-04-03 14:48:22 +0700

    Add Latihan 1 + 2 Modul 3

---
+++ src/Modul3/Latihan/ExampleStack.java
@@ -0,0 +1,30 @@
+package Modul3.Latihan;
+
+import java.util.Stack;
+
+public class ExampleStack {
+    public static void main(String[] args) {
+        Stack<String> st = new Stack<>();
+
+        st.push("Aku");
+        st.push("Anak");
+        st.push("Indonesia");
+
+        System.out.println("Next element: " + st.peek());
+        st.push("Raya");
+        System.out.println(st.pop());
+        st.push("!");
+
+        int count = st.search("Aku");
+        while (count != -1 && count > 1) {
+            st.pop();
+            count--;
+        }
+
+        System.out.println(st.pop());
+        System.out.println(st.empty());
+
+
+    }
+}
+
